6167805761b2cadcdeecd9b06ae5d45c74b34755
Ilona <ilonabarashkina@gmail.com>
2019-06-03 19:16:40 -0400

    Adjusted the route

--- cartsData.js
+++ cartsData.js
@@ -1,34 +1,34 @@
-cartsData = [
-    {
-        id: 1,
-        product:'Potatoes',
-        price: 4.99,
-        quantity:1 
-    },
-    {
-        id: 2,
-        product:'Tomatoes',
-        price: 2.99,
-        quantity: 2
-    },
-    {
-        id: 3,
-        product:'Oranges',
-        price: 3.99,
-        quantity: 1
-    },
-    {
-        id: 4,
-        product:'Onions',
-        price: 1.99,
-        quantity: 1
-    },
-    {
-        id: 5,
-        product:'Bananas',
-        price: 4.59,
-        quantity: 1
-    }
-]
+// cartsData = [
+//     {
+//         id: 1,
+//         product:'Potatoes',
+//         price: 4.99,
+//         quantity:1 
+//     },
+//     {
+//         id: 2,
+//         product:'Tomatoes',
+//         price: 2.99,
+//         quantity: 2
+//     },
+//     {
+//         id: 3,
+//         product:'Oranges',
+//         price: 3.99,
+//         quantity: 1
+//     },
+//     {
+//         id: 4,
+//         product:'Onions',
+//         price: 1.99,
+//         quantity: 1
+//     },
+//     {
+//         id: 5,
+//         product:'Bananas',
+//         price: 4.59,
+//         quantity: 1
+//     }
+// ]
 
-module.exports = cartsData;
+// module.exports = cartsData;
--- routes/cart-items.js
+++ routes/cart-items.js
@@ -1,31 +1,63 @@
 const express = require('express');
-const cartsData = require('../cartsData');
-const carts = express.Router();
+// const cartsData = require('../cartsData');
+const router = express.Router();
 
+cartsData = [
+    {
+        id: 1,
+        product:'Potatoes',
+        price: 4.99,
+        quantity:1 
+    },
+    {
+        id: 2,
+        product:'Tomatoes',
+        price: 2.99,
+        quantity: 2
+    },
+    {
+        id: 3,
+        product:'Oranges',
+        price: 3.99,
+        quantity: 1
+    },
+    {
+        id: 4,
+        product:'Onions',
+        price: 1.99,
+        quantity: 1
+    },
+    {
+        id: 5,
+        product:'Bananas',
+        price: 4.59,
+        quantity: 1
+    }
+]
 
-carts.get('/', (req, res) => {
-    console.log(req.body);
+router.get('/', (req, res) => {
+    // console.log(req.body);
     res.json({data:cartsData});
 });
 
 // creates a new item
-carts.post('/', (req,res) => {
-    const upperName = req.body.name.toUpperCase();
+router.post('/', (req,res) => {
+    // const upperName = req.body.name.toUpperCase();
     console.log(req.body); 
-    res.send({data: upperName});
+    // res.send({data: upperName});
 });
 
 // updates a specific item based on id
 
-carts.put('/:id', (req,res) => {
-    console.log(req.param.id);
-    res.send(res.body);
+router.put('/:id', (req,res) => {
+    console.log(req.params.id,req.body);
+    // res.send(res.body);
 });
 
 // deletes a specific item based on id
-carts.delete('/:id', (req,res) => {
+router.delete('/:id', (req,res) => {
     console.log(req.params.id);
-    res.send(res.body);
+    // res.send(res.body);
 });
 
-module.exports = carts;
+module.exports = router;
